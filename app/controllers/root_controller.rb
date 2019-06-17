6c28e3e25ec6006e4e92792db86c386223d5c8f8
Aaron Michal <amichal@greenriver.com>
2019-06-17 15:52:50 -0400

    health check routes

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,20 @@
 class RootController < ApplicationController
   def index
   end
+
+  def health_check
+    render plain: 'OK'
+  end
+
+  def notifier_check
+    raise 'test error notification'
+  end
+
+  def timeout_check
+    logger.error 'entering timeout_check and burning CPU till we get killed'
+    loop do
+      # spin endlesslly
+    end
+    raise 'should never get here'
+  end
 end
