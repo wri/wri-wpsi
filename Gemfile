4e2d07b507c5847799b103e85b780f5f2cfc9f42
Aaron Michal <amichal@greenriver.com>
2019-06-17 15:31:06 -0400

    Add bundle-audit,brakeman,simplecov and include it CI build

--- Gemfile
+++ Gemfile
@@ -43,6 +43,12 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
+
+  gem 'brakeman', require: false
+  gem 'bundle-audit', require: false
+  gem 'simplecov'
+  gem 'simplecov-console'
 end
 
 group :development do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'simplecov'
+require 'simplecov-console'
+if ENV['COVERAGE'].present?
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::Console,
+  ])
+  SimpleCov.start 'rails'
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
