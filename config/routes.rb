4efd5600a0992b32a634659a26b23d763296aa4f
Aaron Michal <amichal@greenriver.com>
2019-06-17 15:54:08 -0400

    Right path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'root#index'
 
   # default AWS ELB health check path
-  get 'health-check', to: 'home#health_check'
+  get 'health-check', to: 'root#health_check'
   # a check to force an exception notification
-  get 'notifier-check', to: 'home#notifier_check'
+  get 'notifier-check', to: 'root#notifier_check'
   # a check to force a timeout WARNING: burns CPU!
-  get 'timeout-check', to: 'home#timeout_check'
+  get 'timeout-check', to: 'root#timeout_check'
 end
