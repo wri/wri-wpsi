e462e4ebd45f8bacecc6eb575bb61913fc9ceb07
Lucas Braun <lbraun@greenriver.com>
2019-06-17 16:26:52 -0400

    Move Sentry DSN to heroku environment variable

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,4 @@
 Raven.configure do |config|
-  config.dsn = 'https://c6d001157f3e4385a473219a8a374e1a:9838170e0a084bc4b70cbd9bd4f60690@sentry.io/1484102'
+  # No configuration needed for now
+  # DSN set as an environment variable (export SENTRY_DSN=http://public@example.com/project-id)
 end
