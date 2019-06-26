e505e722d04cde3436bc866c0dab838285e0a21b
Lucas Braun <lbraun@greenriver.com>
2019-06-26 11:16:02 -0400

    Refactor map some more, add zoom to layer method

--- app/javascript/components/MainMap.jsx
+++ app/javascript/components/MainMap.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Map, TileLayer, ZoomControl, LayersControl, FeatureGroup } from 'react-leaflet'
-import Legend from 'components/Legend'
+import { Map, TileLayer, ZoomControl, LayersControl } from 'react-leaflet'
 import SearchBox from 'components/SearchBox'
 import RegionInfoBox from 'components/RegionInfoBox'
 import ConflictRiskLayer from 'components/ConflictRiskLayer'
@@ -64,6 +63,7 @@ class MainMap extends React.Component {
       'ETH',
       'SOM',
       'KEN',
+      // 'IND',
     ]
   }
 
@@ -77,49 +77,31 @@ class MainMap extends React.Component {
       .then(data => this.setState({ data }))
   }
 
-  getRiskColor(risk) {
-      return risk >= 1 ?   '#800026' :
-             risk >= 0.9 ? '#BD0026' :
-             risk >= 0.8 ? '#E31A1C' :
-             risk >= 0.7 ? '#FC4E2A' :
-             risk >= 0.6 ? '#FD8D3C' :
-             risk >= 0.5 ? '#FEB24C' :
-             risk >= 0.4 ? '#FED976' :
-                           '#FFEDA0';
-  }
-
   handleCountrySelection(selectedRegionGid0) {
-    this.setState({
-      selectedRegionGid0: selectedRegionGid0,
-    })
+    this.setState({selectedRegionGid0: selectedRegionGid0})
   }
 
   handleRegionSelection(selectedRegionGid2) {
-    this.setState({
-      selectedRegionGid2: selectedRegionGid2,
-    })
+    this.setState({selectedRegionGid2: selectedRegionGid2})
   }
 
   handleLayerSelection(selectedLayer) {
     selectedLayer.bringToFront()
+    this.setState({selectedLayer: selectedLayer})
+  }
 
-    this.setState({
-      selectedLayer: selectedLayer,
-    })
+  zoomToLayer(layer) {
+    this.setState({mapBounds: layer.getBounds()})
   }
 
   clickToFeature(e) {
     const layer = e.target
-
-    // console.log(e.target.getBounds())
     this.handleLayerSelection(layer)
     this.handleRegionSelection(layer.feature.properties.gid_2)
   }
 
   onEachFeature(feature, layer) {
     layer.on({
-      // mouseover: this.highlightFeature.bind(this),
-      // mouseout: this.resetHighlight.bind(this),
       click: this.clickToFeature.bind(this),
     })
   }
@@ -162,6 +144,7 @@ class MainMap extends React.Component {
     return <React.Fragment>
       <Map
         center={position}
+        bounds={this.state.mapBounds}
         zoomControl={false}
         zoom={this.state.zoom}
         style={{height: 800}}
@@ -172,17 +155,12 @@ class MainMap extends React.Component {
           {this.renderBasemaps()}
 
           <LayersControl.Overlay name='Conflict risk model output' checked={true}>
-            <FeatureGroup>
-              <ConflictRiskLayer
-                features={features}
-                selectedRegionGid0={this.state.selectedRegionGid0}
-                selectedRegionGid2={this.state.selectedRegionGid2}
-                onEachFeature={this.onEachFeature.bind(this)}
-                getRiskColor={this.getRiskColor}
-              />
-
-              <Legend title='Risk of Conflict' getColor={this.getRiskColor} />
-            </FeatureGroup>
+            <ConflictRiskLayer
+              features={features}
+              selectedRegionGid0={this.state.selectedRegionGid0}
+              selectedRegionGid2={this.state.selectedRegionGid2}
+              onEachFeature={this.onEachFeature.bind(this)}
+            />
           </LayersControl.Overlay>
         </LayersControl>
       </Map>
