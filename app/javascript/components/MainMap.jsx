1fdcdce8787bede761459ee2b3a861d8fc316d0b
Lucas Braun <lbraun@greenriver.com>
2019-07-08 10:12:17 -0400

    Change all URLs to use https

--- app/javascript/components/MainMap.jsx
+++ app/javascript/components/MainMap.jsx
@@ -128,12 +128,12 @@ class MainMap extends React.Component {
 
   renderBasemaps() {
     const maxZoom = 19
-    const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     const basemaps = [
-      {name: 'Positron',                         url: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'              },
-      {name: 'Dark Matter',                      url: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'               },
-      {name: 'Positron (No Labels)',             url: 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'         },
-      {name: 'Dark Matter (No Labels)',          url: 'http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png'          },
+      {name: 'Positron',                         url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'             },
+      {name: 'Dark Matter',                      url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'              },
+      {name: 'Positron (No Labels)',             url: 'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'        },
+      {name: 'Dark Matter (No Labels)',          url: 'https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png'         },
       {name: 'CartoDB World Antique',            url: 'https://cartocdn_{s}.global.ssl.fastly.net/base-antique/{z}/{x}/{y}.png' },
       {name: 'CartoDB World Eco',                url: 'https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png'     },
       {name: 'CartoDB World Flat Blue',          url: 'https://cartocdn_{s}.global.ssl.fastly.net/base-flatblue/{z}/{x}/{y}.png'},
