f38c85a8c217252dc68411ec34c2206a2a2ebd77
Lucas Braun <lbraun@greenriver.com>
2019-07-30 11:23:32 -0400

    Move dataset list definition to it's own file

---
+++ app/javascript/components/datasets.js
@@ -0,0 +1,39 @@
+const LAYERS = [
+  {id: '107b72a6-6a52-4c8e-a261-d01706627322', name: 'WPSI Risk of Conflict Model Output'},
+  {id: '16a5729f-0f2e-4cd6-84bc-0f72c9132dda', name: 'Global Power Plant Database'},
+  {id: '969fe99d-b861-46a1-8c8e-7c44cbafd1d6', name: 'Population Density (Grid, 1 km)'},
+  {id: '2a694289-fec9-4bfe-a6d2-56c3864ec349', name: 'Recent Internal Displacement'},
+  {id: '3a52f7ed-0c20-4691-981b-3a3521b069aa', name: 'Food Price Spikes'},
+  {id: '1ee0784f-654f-4caf-a059-f03dc46724b6', name: 'Food Price Spikes'},
+  {id: '253437ed-1e21-4544-8bb3-1ec2d2656522', name: 'Food Price Spikes'},
+  {id: '2c7ddb1a-6a21-4f6c-a5a3-696e92bcf053', name: 'Food Price Spikes'},
+  {id: '43804fe4-8c9a-42fc-8c85-2a494727cf3d', name: 'Food Price Spikes'},
+]
+
+const DATASETS = [
+  {name: 'Local Population Density (Grid, 1 km)',             id: '141ef6ab-eed9-4081-8066-7be364a48af0'},
+  {name: 'National Infant Mortality',                         id: '6cfd6255-609e-4922-8709-5aaad0db9fae'},
+  {name: 'Employment in Agriculture (% of Total Employment)', id: 'cc354f7f-2622-44cb-91c1-73559373de72'},
+  {name: 'Agriculture Value added to GDP (% of Total GDP)',   id: '95b013a3-389a-4367-83b7-c9d68c28c406'},
+  {name: 'Food Price Spikes',                                 id: 'acf42a1b-104b-4f81-acd0-549f805873fb'},
+  {name: '2000 Barley Yield Gap (t/ha gap) ',                 id: 'bc2c749d-49a0-46c0-9582-eb33946a983e'},
+  {name: '2000 Maize Yield Gap (t/ha gap)',                   id: 'bc2c749d-49a0-46c0-9582-eb33946a983e'},
+  {name: '2000 Rice Yield Gap (t/ha gap) ',                   id: 'bc2c749d-49a0-46c0-9582-eb33946a983e'},
+  {name: '2000 Soybean Yield Gap (t/ha gap)',                 id: 'bc2c749d-49a0-46c0-9582-eb33946a983e'},
+  {name: 'Cropland Area',                                     id: '0ce24533-7877-4926-b962-a6c726332d82'},
+  {name: 'Pasture Area ',                                     id: '0ce24533-7877-4926-b962-a6c726332d82'},
+  {name: 'WPSI Risk of Conflict Model Output ',               id: '0c3dfe3b-2cd5-4125-ac84-9ce0a73f34b3'},
+  {name: 'Asylum Requests by Destination ',                   id: 'c856396d-d0f2-4aae-9671-4903b2ebed4d'},
+  {name: 'Asylum Requests by Origin',                         id: '7a8b5296-d283-4832-9be1-edd760bbb58f'},
+  {name: 'Recent Internal Displacement by Event',             id: 'd2f6245d-5b9b-4508-874b-d42a2be7d058'},
+  {name: 'Baseline Water Stress',                             id: 'c66d7f3a-d1a8-488f-af8b-302b0f2c3840'},
+  {name: 'Airports ',                                         id: '51159bdb-4904-4101-a88e-ca7bd4f67cb0'},
+  {name: 'Access to Cities ',                                 id: 'ccbcaf7b-1619-4298-8275-b135d1e8e04e'},
+  {name: 'Dams ',                                             id: 'e63bb157-4b98-4ecb-81d6-c1b15e79895a'},
+  {name: 'Ports',                                             id: 'e0b94aef-4a3c-4fe3-8a58-7b713a4234a2'},
+  {name: 'Power Generation by Fuel Type',                     id: 'a86d906d-9862-4783-9e30-cdb68cd808b8'},
+  {name: 'Reservoirs ',                                       id: 'e63bb157-4b98-4ecb-81d6-c1b15e79895a'},
+  {name: 'Roads',                                             id: '943f3702-421f-47a4-9b99-e2d5a9f544a7'},
+]
+
+export { LAYERS, DATASETS };
