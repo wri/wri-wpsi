de677b1a9581b95b0f34a3d81f7f64643e9cd776
Lucas Braun <lbraun@greenriver.com>
2019-09-18 14:18:54 -0400

    Fix bug where tinymce div was unintentionally covering the page

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom'
 import App from '../components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const container = document.createElement('div')
+  container.className = 'container'
+
   ReactDOM.render(
     <App/>,
-    document.body.appendChild(document.createElement('div')),
+    document.body.appendChild(container),
   )
 })
