a00085f281644489e59b62481cbb71e27b667690
Lucas Braun <lbraun@greenriver.com>
2019-09-23 13:54:37 -0400

    Fix SQL bug

--- app/controllers/api/v1/widget_datapoints_controller.rb
+++ app/controllers/api/v1/widget_datapoints_controller.rb
@@ -2,8 +2,9 @@ class Api::V1::WidgetDatapointsController < ApplicationController
   def index
     gid_2 = params.require(:gid_2)
     field_name = params.require(:field_name)
+    quote_column_name = WidgetDatapoint.connection.quote_column_name(field_name)
     widget_datapoints = WidgetDatapoint.where(gid_2: gid_2).
-      where(%(? IS NOT NULL), field_name).
+      where("#{quote_column_name} IS NOT NULL").
       serialized_for_react_app(field_name)
 
     render json: {
