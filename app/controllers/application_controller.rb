400ae649abcf8841b671c8a04fdbcb85f956959f
Lucas Braun <lbraun@greenriver.com>
2019-10-11 15:49:46 -0400

    Fix admin sign-in experience
    After Julie pointed out its flaws ^.^

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
       password: ENV['HTTP_AUTH_PASSWORD'] || 'test',
     )
   end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || admin_path
+  end
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
 end
