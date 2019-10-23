9e7a872f23a8581736951cc8cf8fa1b0bfd5dee3
Lucas Braun <lbraun@greenriver.com>
2019-10-23 12:53:19 -0400

    Fix static page layout to handle content of all sizes

--- app/javascript/components/StaticPage.jsx
+++ app/javascript/components/StaticPage.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import mainStyles from 'components/styles/main_styles'
+import injectSheet from 'react-jss'
 
-const StaticPage = ({ page }) => {
-  return <main style={mainStyles()}>
-    <content dangerouslySetInnerHTML={{__html: page.content}} />
-  </main>
+const styles = {
+  main: {
+    height: '100%',
+    overflow: 'auto',
+  },
+  contentContainer: {
+    maxWidth: '800px',
+    margin: '20px auto',
+    padding: '20px',
+    flex: '1 1 auto',
+  },
+}
+
+const StaticPage = ({ page, classes }) => {
+  return (
+    <main className={classes.main}>
+      <div className={classes.contentContainer}>
+        <content dangerouslySetInnerHTML={{__html: page.content}} />
+      </div>
+    </main>
+  )
 }
 
 import PropTypes from 'prop-types'
@@ -14,6 +31,7 @@ StaticPage.propTypes = {
     slug: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
   }).isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
-export default StaticPage
+export default injectSheet(styles)(StaticPage)
