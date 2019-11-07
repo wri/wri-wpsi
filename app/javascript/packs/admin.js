e61a0ebc3fafd04f1965d5d4c1ab1ae8221a3386
Lucas Braun <lbraun@greenriver.com>
2019-11-07 10:35:30 -0500

    Add widget preview to layer edit view, open api to vega editor site

---
+++ app/javascript/packs/admin.js
@@ -0,0 +1,25 @@
+// This logic renders a live-updating preview of a layer's widget on the layer
+// form partial.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Widget from 'components/Widget'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const widgetSpecEl = document.getElementById('layer_widget_spec')
+  const previewRegionEl = document.getElementById('preview_region')
+
+  if (widgetSpecEl && previewRegionEl) {
+    const updatePreview = () => {
+      ReactDOM.render(
+        <Widget region={{gid_2: previewRegionEl.value}} widgetSpec={JSON.parse(widgetSpecEl.value)} />,
+        document.getElementById('vega-preview'),
+      )
+    }
+
+    widgetSpecEl.addEventListener("input", updatePreview)
+    previewRegionEl.addEventListener("input", updatePreview)
+
+    updatePreview()
+  }
+})
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,9 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'https://vega.github.io',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
 end
