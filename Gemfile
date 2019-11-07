5d8d86bbe0797973265699c34e564dedf21b38ef
Aaron Michal <amichal@greenriver.com>
2019-11-06 22:08:33 -0500

    manage puma + ngnix with capistrano too

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ group :development do
   gem 'capistrano-rvm', require: false
   gem 'capistrano-bundler', require: false
   gem 'capistrano-rails', require: false
+  gem 'capistrano3-puma',   require: false
 end
 
 group :test do
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,29 +2,19 @@
 lock "~> 3.11.2"
 
 set :application, "wri-wpsi"
-set :repo_url, "https://a%2Bwri-wpsi%40greenriver.org:s2GC3fbF1BNoeGXf@github.com/greenriver/wri-wpsi.git"
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
+#set :repo_url, "https://a%2Bwri-wpsi%40greenriver.org:s2GC3fbF1BNoeGXf@github.com/greenriver/wri-wpsi.git"
+set :repo_url, "git@github.com:greenriver/wri-wpsi.git"
 
 set :rvm_custom_path, '/usr/share/rvm'
 set :rvm_ruby_version, '2.6.5'
 
+set :puma_init_active_record, true
+
+# FIXME: remove to switch back to capistrano-puma`s default unix socket setp
+set :puma_bind, %w(tcp://0.0.0.0:3000)
+
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, ".env.production"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
