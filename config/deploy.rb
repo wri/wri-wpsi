df7a092e3ab92569d23d380e871ab72462df0b44
Aaron Michal <amichal@greenriver.com>
2019-11-08 13:52:46 -0500

    Real SSL config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,8 @@ set :rvm_ruby_version, '2.6.5'
 
 set :puma_init_active_record, true
 
-set :nginx_ssl_certificate, '/etc/ssl/certs/ssl-cert-snakeoil.pem'
-set :nginx_ssl_certificate_key, '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+set :nginx_ssl_certificate, '/etc/ssl/certs/waterpeacesecurity_org.crt'
+set :nginx_ssl_certificate_key, '/etc/ssl/private/waterpeacesecurity_org.key'
 set :nginx_use_ssl, true
   # Default value for :linked_files is []
 append :linked_files, ".env.production"
