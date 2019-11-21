6f24ba4c7ee63f08251da52fdb8f3852b5ab6d24
Lucas Braun <lbraun@greenriver.com>
2019-11-21 11:47:25 -0500

    Remove data download link from widget container

--- app/javascript/components/WidgetContainer.jsx
+++ app/javascript/components/WidgetContainer.jsx
@@ -12,34 +12,16 @@ const styles = {
     borderTop: `1px solid ${colors.gray2}`,
     display: 'flex',
     justifyContent: 'center',
-  },
-  linkContainer: {
-    padding: '15px',
-  },
-  downloadLink: {
-    textDecoration: 'none',
-    marginLeft: 'auto',
-  },
+  }
 }
 
 const WidgetContainer = ({ layer, region, classes }) => {
   return (
-    <div>
-      <div className={classes.widgetContainer}>
-        <Widget
-          region={region}
-          widgetSpec={layer.widget_spec}
-        />
-      </div>
-      <div className={classes.linkContainer}>
-        <a
-          href={`/api/v1/widget_datapoints/${region.gid_2}/all/csv`}
-          className={classes.downloadLink}
-        >
-          <i className='icon__download' style={{marginRight: 5}} />
-          <span>Download all data for this region</span>
-        </a>
-      </div>
+    <div className={classes.widgetContainer}>
+      <Widget
+        region={region}
+        widgetSpec={layer.widget_spec}
+      />
     </div>
   )
 }
