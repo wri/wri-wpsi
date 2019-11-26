7e2eff33692aa1abb825ea2d2f3f274e7ace52d4
Lucas Braun <lbraun@greenriver.com>
2019-11-26 11:29:49 -0500

    Handle layers without widgets by showing a message

--- app/javascript/components/WidgetContainer.jsx
+++ app/javascript/components/WidgetContainer.jsx
@@ -12,18 +12,34 @@ const styles = {
     borderTop: `1px solid ${colors.gray2}`,
     display: 'flex',
     justifyContent: 'center',
-  }
+  },
+  noWidgetMessage: {
+    fontStyle: 'italic',
+    fontSize: '13px',
+    lineHeight: '1.2',
+    paddingBottom: '0px',
+  },
 }
 
 const WidgetContainer = ({ layer, region, classes }) => {
-  return (
-    <div className={classes.widgetContainer}>
-      <Widget
-        region={region}
-        widgetSpec={layer.widget_spec}
-      />
-    </div>
-  )
+  if (layer.widget_spec && layer.widget_spec != '') {
+    return (
+      <div className={classes.widgetContainer}>
+        <Widget
+          region={region}
+          widgetSpec={layer.widget_spec}
+        />
+      </div>
+    )
+  } else {
+    const classNames = [classes.widgetContainer, classes.noWidgetMessage]
+    return (
+      <div className={classNames.join(' ')}>
+        The dataset is not used as an input to the model
+        and therefore is not available at the administrative level.
+      </div>
+    )
+  }
 }
 
 import PropTypes from 'prop-types'
