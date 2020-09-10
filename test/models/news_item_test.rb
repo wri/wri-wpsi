6e37687cf8cd7728c989e0239de14748831120ef
Lucas Braun <lbraun@greenriver.com>
2020-09-10 11:11:43 -0400

    Add tests to make sure validations are working
    (and that fixtures/factory are valid).

--- test/models/news_item_test.rb
+++ test/models/news_item_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class NewsItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'fixtures are valid' do
+    NewsItem.all.each do |news_item|
+      news_item.valid?
+      assert_empty(news_item.errors)
+    end
+  end
+
+  test 'factory is valid' do
+    news_item = build(:news_item)
+    news_item.valid?
+    assert_empty(news_item.errors)
+  end
 end
