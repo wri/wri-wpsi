9053781a2524a6440f82611abadc52a3fc54adf6
Lucas Braun <lbraun@greenriver.com>
2020-09-10 11:08:55 -0400

    Remove un-needed comments

--- app/controllers/admin/news_items_controller.rb
+++ app/controllers/admin/news_items_controller.rb
@@ -1,23 +1,18 @@
 class Admin::NewsItemsController < Admin::BaseController
   before_action :set_news_item, only: %i[show edit update]
 
-  # GET /news_items
   def index
     @news_items = NewsItem.all
   end
 
-  # GET /news_items/1
   def show; end
 
-  # GET /news_items/new
   # def new
   #   @news_item = NewsItem.new
   # end
 
-  # GET /news_items/1/edit
   def edit; end
 
-  # POST /news_items
   # def create
   #   @news_item = NewsItem.new(news_item_params)
 
@@ -28,7 +23,6 @@ class Admin::NewsItemsController < Admin::BaseController
   #   end
   # end
 
-  # PATCH/PUT /news_items/1
   def update
     if @news_item.update(news_item_params)
       redirect_to [:admin, @news_item], notice: 'News item was successfully updated.'
@@ -37,7 +31,6 @@ class Admin::NewsItemsController < Admin::BaseController
     end
   end
 
-  # DELETE /news_items/1
   # def destroy
   #   @news_item.destroy
   #   redirect_to news_items_url, notice: 'News item was successfully destroyed.'
@@ -45,12 +38,10 @@ class Admin::NewsItemsController < Admin::BaseController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_news_item
     @news_item = NewsItem.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def news_item_params
     params.require(:news_item).permit(
       :title,
--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,7 +1,6 @@
 class NewsItem < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
-  # is allow_blank necessary? test it
   validates :article_url, url: { allow_blank: false }
   validates :image_url, presence: true
   validates :image_alt_text, presence: true
