9bfe6923e73c45f858eec596af156bdb5a9fa4b8
Liela Rotschy <lrotschy@greenriver.org>
2020-10-07 11:02:46 -0600

    @wip Move javascript

--- app/assets/javascripts/admin/pages.js
+++ app/assets/javascripts/admin/pages.js
@@ -1,13 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-document.addEventListener('DOMContentLoaded', event => {
+document.addEventListener('paste', event => {
   remove_extra_paragraphs();
   replace_p_elems_inside_li();
 });
 
 function remove_extra_paragraphs() {
-  extra_paragraphs = document.querySelectorAll('p');
+  edit_area = document.querySelector(".tox-edit-area")
+  extra_paragraphs = edit_area.querySelectorAll('p');
   for(let i = 0; i < extra_paragraphs.length; i += 1) {
     if (extra_paragraphs[i].innerHTML === "&nbsp;") {
       console.log(extra_paragraphs[i])
@@ -17,7 +18,8 @@ function remove_extra_paragraphs() {
 }
 
 function replace_p_elems_inside_li() {
-  li_elems = document.querySelectorAll('li');
+  edit_area = document.querySelector(".tox-edit-area")
+  li_elems = edit_area.querySelectorAll('li');
   for(let i = 0; i < li_elems.length; i += 1) {
     let children =  li_elems[i].children
     console.log(children)
