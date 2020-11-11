094faddf6d5c9c7fb0a2abd7dc5efc78786b8760
Liela Rotschy <lrotschy@greenriver.org>
2020-11-11 10:02:28 -0500

    Add file url to index

--- test/system/admin/file_uploads.rb
+++ test/system/admin/file_uploads.rb
@@ -5,6 +5,7 @@ class FileUploadsTest < ApplicationSystemTestCase
 
   setup do
     sign_in create(:user)
+    @file_path = File.join(ActionController::TestCase.fixture_path, 'files/gr_logo.png')
   end
 
   test 'navigating to file uploads' do
@@ -15,8 +16,12 @@ class FileUploadsTest < ApplicationSystemTestCase
   end
 
   test 'visiting the index' do
+    file_upload = create(:file_upload, description: 'Test this file')
+    file_upload.file.attach(io: File.open(@file_path), filename: 'gr_logo.png')
     visit admin_file_uploads_url
     assert_selector 'h2', text: 'Listing file uploads'
+    assert_selector 'td', text: 'Test this file'
+    assert_selector 'td', text: file_upload.file.filename.to_s
   end
 
   test 'creating a file upload' do
@@ -24,8 +29,7 @@ class FileUploadsTest < ApplicationSystemTestCase
     click_on 'Upload a new file'
 
     fill_in 'Description', with: 'This is a description'
-    file_path = File.join(ActionController::TestCase.fixture_path, 'files/gr_logo.png')
-    attach_file 'file_upload[file]', file_path
+    attach_file 'file_upload[file]', @file_path
     click_on I18n.t('helpers.submit.file_upload.create')
 
     assert_text 'File upload was successfully created'
