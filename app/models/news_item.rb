b31aec90090606ed377c744be078ca5c57b4c5f6
Liela Rotschy <lrotschy@greenriver.org>
2020-11-18 15:29:13 -0500

    Revert changes to url and validations and turn off rubocop

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,8 +1,8 @@
 class NewsItem < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
-  validates :article_url, presence: true
-  validates :image_url, presence: true
+  validates :article_url, url: { allow_blank: false }
+  validates :image_url, url: { allow_blank: false }
   validates :image_alt_text, presence: true
   validates :date, presence: true
 end
