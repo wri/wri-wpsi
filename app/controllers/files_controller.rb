f4103559aa1d3d53c69a07acb95336637c38e7d7
Lucas Braun <lbraun@greenriver.com>
2021-03-31 18:04:04 -0400

    Make files stream inline instead of redirecting
    Followed example of section on streaming here:
    https://nts.strzibny.name/displaying-downloading-streaming-files-with-active-storage/

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,6 +1,26 @@
 class FilesController < ApplicationController
   def show
-    file_upload = FileUpload.find(params[:id])
-    redirect_to url_for(file_upload.file)
+    load_file_upload
+
+    response.headers['Content-Type'] = content_type_header
+    response.headers['Content-Disposition'] = content_disposition_header
+
+    @file_upload.file.download do |chunk|
+      response.stream.write(chunk)
+    end
+  end
+
+  private
+
+  def load_file_upload
+    @file_upload = FileUpload.find(params[:id])
+  end
+
+  def content_type_header
+    @file_upload.file.content_type
+  end
+
+  def content_disposition_header
+    "inline; #{@file_upload.file.filename.parameters}"
   end
 end
--- test/controllers/files_controller_test.rb
+++ test/controllers/files_controller_test.rb
@@ -4,6 +4,6 @@ class FilesControllerTest < ActionDispatch::IntegrationTest
   test 'should get show' do
     file_upload = create(:file_upload)
     get file_url(file_upload.id)
-    assert_response :redirect
+    assert_response :ok
   end
 end
