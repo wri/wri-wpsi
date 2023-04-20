0e894e39c76007d0670e797882adeec8be4c6746
Carlos Del Real <carloshdelreal@gmail.com>
2023-04-20 15:38:19 -0500

    test: update user count because migrations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'create' do
+    count = User.count
     User.create!(email: 'test@example.com', password: 'test_password')
-    assert_equal 1, User.count
+    assert_equal 1, count + 1
   end
 end
