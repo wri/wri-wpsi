62106ff27f94fe47400c8f57d18a54e6be0f3811
Carlos Del Real <carloshdelreal@gmail.com>
2023-04-21 21:34:37 -0500

    feat: get rid of useless test

--- test/models/user_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UserTest < ActiveSupport::TestCase
-  test 'create' do
-    count = User.count
-    User.create!(email: 'test@example.com', password: 'test_password')
-    assert_equal 1, count + 1
-  end
-end
