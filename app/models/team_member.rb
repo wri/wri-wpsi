5d825b0af3b4e1440cdcd5d3878d839b8412545c
Carlos Del Real <carloshdelreal@gmail.com>
2023-04-25 11:02:47 -0500

    feat: add team members validation and style improvement

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,3 +1,8 @@
 class TeamMember < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :email, presence: true
+  validates :profile_image, presence: true
+
   has_and_belongs_to_many :tags, join_table: :team_member_tags
 end
