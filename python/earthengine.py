ca80aea21bd0f10097bf167d616c0c998d807f1c
Carlos Del Real <carloshdelreal@gmail.com>
2024-11-11 14:04:50 -0500

    feat: venv to test python scripts

--- python/earthengine.py
+++ python/earthengine.py
@@ -1,18 +1,9 @@
-import ee 
-from google.auth import compute_engine, impersonated_credentials
+import ee
+from google.auth import compute_engine
 
 scopes = [
     "https://www.googleapis.com/auth/earthengine"
 ]
 
-# Pay close attention to the scopes flow down...
 credentials = compute_engine.Credentials(scopes=scopes)
-
-delegated = impersonated_credentials.Credentials(
-    source_credentials = credentials,
-    target_principal = 'wps-automation@wpsi-208318.iam.gserviceaccount.com', # email of target service account.
-    target_scopes = scopes,
-    lifetime = 300
-)
-
-ee.Initialize(delegated)
+ee.Initialize(credentials)
--- python/readme.md
+++ python/readme.md
@@ -11,3 +11,10 @@ docker build -f python/docker/Act.Dockerfile -t ubuntu-builder .
 ```bash
 act -P ubuntu-latest=ubuntu-builder --pull=false --container-architecture linux/amd64 --secret-file .secrets workflow_dispatch --workflows '.github/workflows/cron_update_SPI24.yml'
 ```
+
+
+## create venv for running python scripts
+
+```
+python3 -m venv automationvenv
+```
