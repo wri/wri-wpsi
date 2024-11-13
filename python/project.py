3586178ca32d4d5b004b465600f0f4d57ec6567c
Carlos Del Real <carloshdelreal@gmail.com>
2024-11-13 09:13:29 -0500

    feat: eu.upload

--- python/project.py
+++ python/project.py
@@ -18,5 +18,4 @@ with rasterio.open(f"{FILE}{EXTENTION}", "r+") as rds:
 
 eu.init(bucket=BUCKET)
 
-eu.upload([f"{FILE}{EXTENTION}"], [GEE_FILE], gs_prefix='wpsstaging', dates=[], public=True,
-                timeout=300, clean=True, bands=[])
+eu.upload([f"{FILE}{EXTENTION}"], [GEE_FILE], gs_prefix='wpsstaging', public=True)
