074dd8005698e0d9d3ba50b2a0429692334bbb71
Carlos Del Real <carloshdelreal@gmail.com>
2024-11-13 08:36:46 -0500

    feat: default bucket typo

--- python/project.py
+++ python/project.py
@@ -15,6 +15,6 @@ else:
 with rasterio.open(f"{FILE}{EXTENTION}", "r+") as rds:
     rds.crs = CRS.from_epsg(4326)
 
-eu.init(bucket='wps-pillar1a')
+eu.init(bucket='wps_pillar1a')
 
 eu.uploadAsset(f"{FILE}{EXTENTION}", f"projects/wpsi-208318/assets/wpsi/{FILE}", gs_prefix='', date='', public=True)
