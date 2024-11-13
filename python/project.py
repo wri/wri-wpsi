efc6a5e130bf203e6f9a882d4f50b9a5de4dbf79
Carlos Del Real <carloshdelreal@gmail.com>
2024-11-13 08:48:25 -0500

    feat: default bucket typo, setAlc

--- python/project.py
+++ python/project.py
@@ -5,7 +5,8 @@ import eeUtil as eu
 
 FILE = os.environ["WPS_24_MONTH_FILENAME"]
 EXTENTION = ".tif"
-
+BUCKET ='wps_pillar1a'
+GEE_FILE = f"projects/wpsi-208318/assets/wpsi/{FILE}"
 
 if FILE:
     print('The filename evn WPS_24_MONTH_FILENAME={FILE} successfully loaded!')
@@ -15,6 +16,8 @@ else:
 with rasterio.open(f"{FILE}{EXTENTION}", "r+") as rds:
     rds.crs = CRS.from_epsg(4326)
 
-eu.init(bucket='wps_pillar1a')
+eu.init(bucket=BUCKET)
 
-eu.uploadAsset(f"{FILE}{EXTENTION}", f"projects/wpsi-208318/assets/wpsi/{FILE}", gs_prefix='', date='', public=True)
+eu.uploadAsset(f"{FILE}{EXTENTION}", GEE_FILE)
+
+eu.setAcl(GEE_FILE, 'public')
