2631c02af32b55bd0501b67c95318fbc780a8003
Carlos Del Real <carloshdelreal@gmail.com>
2025-07-03 13:50:42 -0500

    feat: fixing ExecJS

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,27 +64,12 @@ after 'deploy:log_revision', :push_deploy_tag
 # Build Next.js application after deployment
 after 'deploy:updated', 'react:build'
 
-# Verify Node.js is available before deployment
-before 'deploy:updated', 'deploy:check_nodejs'
-
-desc 'Check if Node.js is available'
-task 'deploy:check_nodejs' do
-  on roles(:app) do
-    within release_path do
-      puts "Checking Node.js availability..."
-      execute :node, '--version'
-      execute :which, 'node'
-      puts "Node.js check completed"
-    end
-  end
-end
-
 desc 'Restart puma to pick up latest code changes'
 task :restart_puma do
   ruby_version = File.read(".ruby-version").chomp.split("-").last
 
   on roles(:app) do
-    execute "bash -l -c 'cd #{fetch(:release_path)} && rvm #{ruby_version} do bundle exec pumactl -S #{fetch(:deploy_to)}/shared/tmp/pids/puma.state -F #{fetch(:deploy_to)}/shared/puma.rb restart'"
+    execute "bash -l -c 'cd #{fetch(:release_path)} && PATH=/home/ubuntu/.nvm/versions/node/v16.15.0/bin:$PATH rvm #{ruby_version} do bundle exec pumactl -S #{fetch(:deploy_to)}/shared/tmp/pids/puma.state -F #{fetch(:deploy_to)}/shared/puma.rb restart'"
   end
 end
 
